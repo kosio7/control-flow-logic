ae12d880096023dc25e14fe8893fc597f237de2c
Konstantin Stefanov <kosio7@gmail.com>
2016-09-27 11:44:39 +0300

    JS Control Flow Logic

---
+++ .idea/multplyNums.js
@@ -0,0 +1,6 @@
+function mult([num1, num2]) {
+    "use strict;"
+    return num1 * num2
+}
+
+mult(['5', '2']);
---
+++ oddEven.js
@@ -0,0 +1,11 @@
+function oddEven(num) {
+    "use strict";
+    let rem = num % 2
+    if (rem == 0)
+        console.log("even")
+    else if (rem == Math.round(rem))
+        console.log("odd")
+    else console.log("invalid")
+}
+
+oddEven('2');
